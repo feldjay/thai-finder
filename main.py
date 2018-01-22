0f6bc0c2fa7c3ed531a67f96af926af9ecd6fa36
Jenna Feldman <jennafeldman@gmail.com>
2018-01-21 20:49:48 -0500

    Update requirements and procfile

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ import pandas as pd
 
 app = Flask(__name__)
 engine = create_engine('sqlite:///foo.db')
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
-#url = 'https://nycopendata.socrata.com/api/views/xx67-kt59/rows.csv?accessType=DOWNLOAD'
-url = 'restaurants.csv'
+url = 'https://nycopendata.socrata.com/api/views/xx67-kt59/rows.csv?accessType=DOWNLOAD'
 data = pd.read_csv(url)
 acceptable_grades = ['A', 'B']
 thai_restaurants = data.loc[data["CUISINE DESCRIPTION"] == 'Thai']
@@ -29,8 +27,7 @@ def get_restaurants():
             data_dict[rest['BORO']].append(rest['DBA'])
         else:
             data_dict[rest['BORO']] = [rest['DBA']]
-    boros = list(data_dict.keys())
-    return render_template('layout.html', boros=boros, data=data_dict)
+    return render_template('layout.html', data=data_dict)
 
 
 if __name__ == "__main__":
